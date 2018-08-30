00affc96db57104b9f90bf0b9a8cdd2931297895
marcelo046 <marc3lo046@gmail.com>
2018-08-30 18:13:37 -0300

    now the graph has numbers for y

--- op_vectors/plotGraph.h
+++ op_vectors/plotGraph.h
@@ -4,18 +4,23 @@
 //#define DIST_TO_RIGHT_SIDE 20
 
 #define ROP 2 // radius of point (raio do ponto)
-#define LEFT 20
+#define LEFT 30
 #define TOP 40
-#define RIGHT 610
+#define RIGHT 620
 #define BOTTOM 400
 
 // valor maximo dos pontos é 360
 
 void plotGraph(int *vector, int n, char *title, int minisec) {
   int x, dbp = RIGHT/n;
+  char str[5];
   cleardevice(); // apaga tela do grafico
   outtextxy(LEFT + 250, TOP-20, title);
   rectangle(LEFT, TOP, RIGHT, BOTTOM);
+  for (x = 0; x <= BOTTOM-TOP; x+=20) {
+    sprintf(str, "%d", x);
+    outtextxy(0, BOTTOM-x, str);
+  }
   for (x = 0; x < n; x++) {
     circle(x*dbp+LEFT, BOTTOM-vector[x], ROP);
   }
